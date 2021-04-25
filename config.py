e2d6f150835cf13b16e741d97b794e5b3e1f9b23
Glitch (hello-webpage) <none>
2021-04-25 10:49:02 +0200

    fix typo

--- config.py
+++ config.py
@@ -12,4 +12,3 @@ SQLALCHEMY_ECHO = False
 SQLALCHEMY_DATABASE_URI = f"mysql + mysqldb://root:{DB_PASSWORD}@{PUBLIC_IP_ADDRESS}/{DB_NAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-appraisal_report_generator/appraisal_report_app/upload_folder
