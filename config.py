9cf26cbdcf5d3061856644b4986861aff0dbe87e
Glitch (hello-webpage) <none>
2021-04-25 10:46:11 +0200

    fix upload_folder path

--- config.py
+++ config.py
@@ -7,8 +7,9 @@ PUBLIC_IP_ADDRESS= os.environ["PUBLIC_IP_ADDRESS"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_NAME = os.environ["DB_NAME"]
 PROJECT_ID = os.environ["PROJECT_ID"]
-INSTANCE_NAME = os.environ["INCTANCE_NAME"]
+INSTANCE_NAME = os.environ["INSTANCE_NAME"]
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_DATABASE_URI = f"mysql + mysqldb://root:{DB_PASSWORD}@{PUBLIC_IP_ADDRESS}/{DB_NAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+appraisal_report_generator/appraisal_report_app/upload_folder
