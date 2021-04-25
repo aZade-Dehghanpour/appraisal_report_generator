965b6557cf95cf7a681c8b977894cf98666dce0e
Glitch (hello-webpage) <none>
2021-04-25 10:16:38 +0200

    set variables for connection to Cloud SQL

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+import os
+
 DEBUG = False
+UPLOAD_FOLDER = os.environ["UPLOAD_FOLDER"] 
+SECRET_KEY = os.environ["SECRET_KEY"]   
+PUBLIC_IP_ADDRESS= os.environ["PUBLIC_IP_ADDRESS"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_NAME = os.environ["DB_NAME"]
+PROJECT_ID = os.environ["PROJECT_ID"]
+INCTANCE_NAME = os.environ["INCTANCE_NAME"]
 SQLALCHEMY_ECHO = False
-SQLALCHEMY_DATABASE_URI = "sqlite:///reports.db" 
-UPLOAD_FOLDER = "appraisal_report_app/upload_folder/"
+SQLALCHEMY_DATABASE_URI = f"mysql + mysqldb://root:{DB_PASSWORD}@{PUBLIC_IP_ADDRESS}/{DB_NAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
