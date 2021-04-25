c835bf8eea15dd36996dee73b692a3f8d8f8c2f8
Glitch (hello-webpage) <none>
2021-04-25 11:48:14 +0200

    decrypt project id

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 PUBLIC_IP_ADDRESS= os.getenv("PUBLIC_IP_ADDRESS")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
-PROJECT_ID = os.getenv("PROJECT_ID")
+PROJECT_ID = "appraisal-report-app"
 INSTANCE_NAME = os.getenv("INSTANCE_NAME")
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_DATABASE_URI = f"mysql + mysqldb://root:{DB_PASSWORD}@{PUBLIC_IP_ADDRESS}/{DB_NAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
